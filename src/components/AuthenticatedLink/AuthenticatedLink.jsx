37c8e8baf742e4bb568a93f605627b87e72da5c3
Yossan Rahmadi <yossansr06@gmail.com>
2020-09-14 13:16:28 +0700

    create component to download protected file

---
+++ src/components/AuthenticatedLink/AuthenticatedLink.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prop-types */
+import React, { createRef } from 'react';
+
+export default function AuthenticatedLink({ url, filename, children }) {
+  const link = createRef();
+  const baseUrl = 'http://localhost:4444';
+  const token = localStorage.getItem('token');
+
+  const handleAction = async () => {
+    if (link.current.href) { return; }
+
+    const result = await fetch(baseUrl + url, {
+      headers: { token },
+    });
+
+    const blob = await result.blob();
+    const href = window.URL.createObjectURL(blob);
+
+    link.current.download = filename;
+    link.current.href = href;
+
+    link.current.click();
+  };
+
+  return (
+    <>
+      <a role="button" ref={link} onClick={handleAction}>{children}</a>
+    </>
+  );
+}
