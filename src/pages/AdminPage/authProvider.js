c5bb6dbf9c0dc63698df382d980a1adf53aa6b47
Yossan Rahmadi <yossansr06@gmail.com>
2020-09-15 19:01:41 +0700

    Create auth functionality for Admin

--- src/pages/AdminPage/authProvider.js
+++ src/pages/AdminPage/authProvider.js
@@ -1,16 +1,16 @@
+import fetchAPI from '../../utils/fetchAPI';
+
 export default {
   // called when the user attempts to log in
-  login: ({ username }) => {
-    localStorage.setItem('username', username);
-    // accept all username/password combinations
-    return Promise.resolve();
-  },
+  login: ({ username, password }) => fetchAPI('/admin/login', {}, 'POST', { username, password })
+    .then((resJson) => localStorage.setItem('admin', resJson.data.token))
+    .catch(() => 'Wrong credentials'),
   // called when the user clicks on the logout button
   logout: () => {
-    localStorage.removeItem('username');
+    localStorage.removeItem('admin');
     return Promise.resolve();
   },
-  // called when the API returns an error
+  // // called when the API returns an error
   checkError: ({ status }) => {
     if (status === 401 || status === 403) {
       localStorage.removeItem('username');
@@ -18,10 +18,10 @@ export default {
     }
     return Promise.resolve();
   },
-  // called when the user navigates to a new location, to check for authentication
-  checkAuth: () => (localStorage.getItem('username')
+  // // called when the user navigates to a new location, to check for authentication
+  checkAuth: () => (localStorage.getItem('admin')
     ? Promise.resolve()
     : Promise.reject()),
-  // called when the user navigates to a new location, to check for permissions / roles
+  // // called when the user navigates to a new location, to check for permissions / roles
   getPermissions: () => Promise.resolve(),
 };
