056074915e3171462050604bd17e1c26647dd984
Yossan Rahmadi <yossansr06@gmail.com>
2020-09-15 18:57:50 +0700

    Change folder name and refactor <Switch>

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Skripsi from './pages/Skripsi/Skripsi';
 import Form from './pages/Form/Form';
 import PublicRoute from './components/PublicRoute/PublicRoute';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import userReducer from './contexts/userReducer';
+import userReducer from './reducer/userReducer';
 
 import './App.css';
 import Profile from './pages/Profile/Profile';
@@ -58,64 +58,64 @@ const App = () => {
       <Switch>
         <Route exact path="/admin" component={AdminPage} />
         <PublicRoute restricted exact path="/form" component={Form} />
+      </Switch>
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          width={300}
+        >
+          <Title style={{ textAlign: 'center', padding: '16px', color: '#00A6FB' }} level={3}>E-Skripsi</Title>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<HomeOutlined />}>
+              <Link to="/">
+                Home
+              </Link>
+            </Menu.Item>
+            <SubMenu key="sub2" icon={<UploadOutlined />} title="File">
+              <Menu.Item key="2">
+                <Link to="/skripsi">
+                  Skripsi
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Link to="/jurnal">
+                  Jurnal
+                </Link>
+              </Menu.Item>
+            </SubMenu>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              <Link to="/profile">
+                Profile
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="5">
+              <Button
+                type="primary"
+                icon={<LogoutOutlined />}
+                danger
+                onClick={() => dispatch({
+                  type: 'LOGOUT',
+                })}
+              >
+                Logout
+              </Button>
+            </Menu.Item>
+          </Menu>
+        </Sider>
         <Layout>
-          <Sider
-            breakpoint="lg"
-            collapsedWidth="0"
-            width={300}
-          >
-            <Title style={{ textAlign: 'center', padding: '16px', color: '#00A6FB' }} level={3}>E-Skripsi</Title>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-              <Menu.Item key="1" icon={<HomeOutlined />}>
-                <Link to="/">
-                  Home
-                </Link>
-              </Menu.Item>
-              <SubMenu key="sub2" icon={<UploadOutlined />} title="File">
-                <Menu.Item key="2">
-                  <Link to="/skripsi">
-                    Skripsi
-                  </Link>
-                </Menu.Item>
-                <Menu.Item key="3">
-                  <Link to="/jurnal">
-                    Jurnal
-                  </Link>
-                </Menu.Item>
-              </SubMenu>
-              <Menu.Item key="4" icon={<UserOutlined />}>
-                <Link to="/profile">
-                  Profile
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="5">
-                <Button
-                  type="primary"
-                  icon={<LogoutOutlined />}
-                  danger
-                  onClick={() => dispatch({
-                    type: 'LOGOUT',
-                  })}
-                >
-                  Logout
-                </Button>
-              </Menu.Item>
-            </Menu>
-          </Sider>
-          <Layout>
-            <Content style={{ margin: '24px 16px 0' }}>
-              <div className="site-layout-background" style={{ padding: 24, minHeight: '89vh' }}>
-
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: '89vh' }}>
+              <Switch>
                 <PrivateRoute exact path="/" component={Home} />
                 <PrivateRoute exact path="/skripsi" component={Skripsi} />
                 <PrivateRoute exact path="/profile" component={Profile} />
-
-              </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Teknik Informatika UAD ©2020</Footer>
-          </Layout>
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Teknik Informatika UAD ©2020</Footer>
         </Layout>
-      </Switch>
+      </Layout>
     </AuthContext.Provider>
   );
 };
